82e39f0a3c8f31be32826be574047a1973824735
mdimran1409036 <mdimran1409036@gmail.com>
2021-09-24 05:19:29 +0600

    Order summery working

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import './Cart.css'
 const Cart = (props) => {
-    console.log(props);
+    const {cart} = props;
+    let sumPrice=0;
+    let sumShipping=0;
+    for(const product of cart){
+        sumPrice += product.price
+        sumShipping += product.shipping
+    }
+    const beforeTax = (sumPrice + sumShipping).toFixed(2);
+    const tax = ((sumPrice + sumShipping)*.2).toFixed(2);
+    const grandTotal = (sumPrice + sumShipping + ((sumPrice + sumShipping) * .2)).toFixed(2)
     return (
         <div className="summary">
             <h2 className="summary-title">Order Summary</h2>
@@ -9,24 +18,24 @@ const Cart = (props) => {
             <table >
                 <tbody>
                     <tr>
-                        <td>Items:</td>
-                        <td className='amount'>$579.99</td>
+                        <td>Total:</td>
+                        <td className='amount'>${sumPrice}</td>
                     </tr>
                     <tr>
                         <td>Shipping & Handling:</td>
-                        <td className='amount'>$7.99</td>
+                        <td className='amount'>${sumShipping}</td>
                     </tr>
                     <tr>
                         <td>Total before tax:</td>
-                        <td className='amount'>$579.99</td>
+                        <td className='amount'>${beforeTax}</td>
                     </tr>
                     <tr>
                         <td>Estimated Tax:</td>
-                        <td className='amount'>5.3</td>
+                        <td className='amount'>${tax}</td>
                     </tr>
                     <tr>
                         <td className="font-bold">Order total:</td>
-                        <td className="font-bold amount">$58.28</td>
+                        <td className="font-bold amount">${grandTotal}</td>
                     </tr>
                 </tbody>
             </table>
